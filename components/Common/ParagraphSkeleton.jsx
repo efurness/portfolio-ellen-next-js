0c3375c6acbe0296e4bc28a75b2a454c2c0ff0c5
Austin Heller <me@aheller.com>
2024-04-01 20:04:38 -0500

    Add keys to components

--- components/Common/ParagraphSkeleton.jsx
+++ components/Common/ParagraphSkeleton.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import CardLayout from './CardLayout'
+import React from "react";
+import CardLayout from "./CardLayout";
 
 const ParagraphSkeleton = ({ className }) => {
-    return (
+  return (
+    <CardLayout>
+      <div
+        role="status"
+        className={`${className} max-w-full card_stylings animate-pulse bg-EveningBlack self-center`}
+      >
+        <div
+          key="div_1"
+          className="h-2.5 rounded-full bg-Green/20 w-48 mb-4"
+        ></div>
+        <div
+          key="div_2"
+          className="h-2 rounded-full bg-Green/20 max-w-[365px] mb-2.5"
+        ></div>
+        <div key="div_3" className="h-2 rounded-full bg-Green/20 mb-2.5"></div>
+        <div
+          key="div_4"
+          className="h-2 rounded-full bg-Green/20 max-w-[330px] mb-2.5"
+        ></div>
+        <div
+          key="div_5"
+          className="h-2 rounded-full bg-Green/20 max-w-[400px] mb-2.5"
+        ></div>
+        <div
+          key="div_6"
+          className="h-2 rounded-full bg-Green/20 max-w-[360px]"
+        ></div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    </CardLayout>
+  );
+};
 
-        <CardLayout>
-            <div role="status" class={`${className} max-w-full card_stylings animate-pulse bg-EveningBlack self-center`}>
-                <div class="h-2.5 rounded-full bg-Green/20 w-48 mb-4"></div>
-                <div class="h-2 rounded-full bg-Green/20 max-w-[365px] mb-2.5"></div>
-                <div class="h-2 rounded-full bg-Green/20 mb-2.5"></div>
-                <div class="h-2 rounded-full bg-Green/20 max-w-[330px] mb-2.5"></div>
-                <div class="h-2 rounded-full bg-Green/20 max-w-[400px] mb-2.5"></div>
-                <div class="h-2 rounded-full bg-Green/20 max-w-[360px]"></div>
-                <span class="sr-only">Loading...</span>
-            </div>
-        </CardLayout>
-
-    )
-}
-
-export default ParagraphSkeleton
+export default ParagraphSkeleton;
--- constants/constants.js
+++ constants/constants.js
@@ -1,62 +1,61 @@
-export const NAME = 'Ellen D. Furness';
+export const NAME = "Ellen D. Furness";
 
 export const DESIGNATION = `GIS Specialist | Open Source Contributor | REACT`;
 
 export const DETAILS = {
-  Residence: 'United States',
-  City: 'Boulder',
-  Age: '52',
+  Residence: "United States",
+  City: "Boulder",
+  Age: "52",
 };
 
 export const SKILLS = [
   {
-    title: 'ArcGIS Pro',
-    level: '85%',
+    title: "ArcGIS Pro",
+    level: "85%",
   },
 
   {
-    title: 'Learning Python for ArcGIS Pro',
-    level: '70%',
+    title: "Learning Python for ArcGIS Pro",
+    level: "70%",
   },
   {
-    title: 'Full-Stack Javascript Developer',
-    level: '80%',
+    title: "Full-Stack Javascript Developer",
+    level: "80%",
   },
   {
-    title: 'React/Next.js Developer',
-    level: '90%',
+    title: "React/Next.js Developer",
+    level: "90%",
   },
   {
-    title: 'Biological Researcher',
-    level: '90%',
+    title: "Biological Researcher",
+    level: "90%",
   },
- 
 ];
 
 export const TECH_STACK = [
-  'ArcGIS Pro',
-  'JavaScript API',
-  'Python for ArcGIS',
-  'ReactJS',
-  'JavaScript',
-  'NextJS',
-  'NodeJS',
-  'CSS',
-  'TailwindCSS',
-  'REST API',
-  'MongoDB',
-  'MySQL',
-  'Git',
-  'GitHub',
+  "ArcGIS Pro",
+  "JavaScript API",
+  "Python for ArcGIS",
+  "ReactJS",
+  "JavaScript",
+  "NextJS",
+  "NodeJS",
+  "CSS",
+  "TailwindCSS",
+  "REST API",
+  "MongoDB",
+  "MySQL",
+  "Git",
+  "GitHub",
 ];
 
 export const CONTACTS = {
-  EMAIL: 'iosamajavaid@gmail.com',
-  PHONE: '+923159591822',
+  EMAIL: "iosamajavaid@gmail.com",
+  PHONE: "+923159591822",
 };
 export const SOCIAL_LINKS = {
-  GITHUB: 'https://github.com/efurness',
-  LINKEDIN: 'https://www.linkedin.com/feed/',
+  GITHUB: "https://github.com/efurness",
+  LINKEDIN: "https://www.linkedin.com/feed/",
   // TWITTER: 'https://twitter.com/iosamajavaid',
   // FACEBOOK: 'https://www.facebook.com/iosamajavaid',
 };
