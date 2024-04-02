5212b94c871caa4a9d8dc171e5d84e40950c1acb
Austin Heller <me@aheller.com>
2024-04-01 20:29:36 -0500

    Add keys to components

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -1,30 +1,43 @@
-import Link from 'next/link';
-import { FaDownload, FaFacebook, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
-import Contact from './Contact';
-import Download from './Download';
-import Languages from './Languages';
-import Location from './Location';
-import Tools from './Tools';
-import Skills from './Skills';
-import Image from 'next/image';
-import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import Ellen from '../../../public/images/portfolio2024.png';
+import Link from "next/link";
+import {
+  FaDownload,
+  FaFacebook,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import Contact from "./Contact";
+import Download from "./Download";
+import Languages from "./Languages";
+import Location from "./Location";
+import Tools from "./Tools";
+import Skills from "./Skills";
+import Image from "next/image";
+import { NAME, DESIGNATION, SOCIAL_LINKS } from "../../../constants/constants";
+import Ellen from "../../../public/images/portfolio2024.png";
 
 const Intro = (props) => {
-  console.log(props.theme)
   return (
     <>
       {/* fixed at top */}
-      <div className='header z-50 absolute ${bgColor} backdrop-blur-sm inset-y-0 h-48 top-0 flex items-center justify-center w-full flex-col px-4 gap-y-4'>
-        <Image className='w-20 h-20 rounded-full' src={Ellen} alt='profile picture' />
-        <div className='flex flex-col items-center justify-center'>
-          <span className='${textColor} text-base font-bold break-normal'>{NAME}</span>
-          <span className='text-sm ${textColors} text-center mt-2'>{DESIGNATION}</span>
+      <div className="header z-50 absolute ${bgColor} backdrop-blur-sm inset-y-0 h-48 top-0 flex items-center justify-center w-full flex-col px-4 gap-y-4">
+        <Image
+          className="w-20 h-20 rounded-full"
+          src={Ellen}
+          alt="profile picture"
+        />
+        <div className="flex flex-col items-center justify-center">
+          <span className="${textColor} text-base font-bold break-normal">
+            {NAME}
+          </span>
+          <span className="text-sm ${textColors} text-center mt-2">
+            {DESIGNATION}
+          </span>
         </div>
       </div>
 
       {/* middle components */}
-      <div className='beech z-20 flex flex-col overflow-y-scroll pt-48 top-48 space-y-6 divide-y divide-white overflow-x-hidden no-scrollbar px-4'>
+      <div className="beech z-20 flex flex-col overflow-y-scroll pt-48 top-48 space-y-6 divide-y divide-white overflow-x-hidden no-scrollbar px-4">
         {/* <Location /> */}
         {/* <Languages /> */}
         <Skills />
@@ -34,8 +47,13 @@ const Intro = (props) => {
       </div>
 
       {/* fixed at bottom */}
-      <div className='footer absolute flex justify-center space-x-6 text-xl items-center bottom-0 z-50 h-10 w-full bg-MidNightBlack text-Snow'>
-        <Link href={SOCIAL_LINKS.GITHUB} target='_blank' rel='noreferrer' className=''>
+      <div className="footer absolute flex justify-center space-x-6 text-xl items-center bottom-0 z-50 h-10 w-full bg-MidNightBlack text-Snow">
+        <Link
+          href={SOCIAL_LINKS.GITHUB}
+          target="_blank"
+          rel="noreferrer"
+          className=""
+        >
           <FaGithub />
         </Link>
         {/* <Link href={SOCIAL_LINKS.TWITTER} target='_blank' rel='noreferrer' className=''>
@@ -44,7 +62,12 @@ const Intro = (props) => {
         <Link href={SOCIAL_LINKS.FACEBOOK} target='_blank' rel='noreferrer' className=''>
           <FaFacebook />
         </Link> */}
-        <Link href={SOCIAL_LINKS.LINKEDIN} target='_blank' rel='noreferrer' className=''>
+        <Link
+          href={SOCIAL_LINKS.LINKEDIN}
+          target="_blank"
+          rel="noreferrer"
+          className=""
+        >
           <FaLinkedin />
         </Link>
       </div>
--- pages/layout.js
+++ pages/layout.js
@@ -1,8 +1,8 @@
-import Intro from '../components/Common/Intro';
-import Nav from '../components/Common/Nav/Nav';
-import { FaBars, FaMousePointer } from 'react-icons/fa';
-import { SlOptionsVertical } from 'react-icons/sl';
-import { useState } from 'react';
+import Intro from "../components/Common/Intro";
+import Nav from "../components/Common/Nav/Nav";
+import { FaBars, FaMousePointer } from "react-icons/fa";
+import { SlOptionsVertical } from "react-icons/sl";
+import { useState } from "react";
 import Switch from "react-switch";
 
 export default function Layout({ children }) {
@@ -14,11 +14,12 @@ export default function Layout({ children }) {
 
   const bgColor = theme === "dark" ? "bg-DeepNightBlack" : "bg-sky-100";
 
-  
   return (
-    <div className={`h-screen lg:p-[0.8rem] flex flex-col select-none font-circular`}>
+    <div
+      className={`h-screen lg:p-[0.8rem] flex flex-col select-none font-circular`}
+    >
       <div className="theme-switcher">
-        <label style={{float:"right"}}>
+        <label style={{ float: "right" }}>
           <span>Theme:</span>
           <Switch
             onChange={() => {
@@ -27,56 +28,77 @@ export default function Layout({ children }) {
               } else {
                 setTheme("dark");
               }
-              console.log(theme);
             }}
             checked={theme === "dark" ? true : false}
-            
           />
         </label>
       </div>
-      
-      <div className='lg:hidden'>
-        <div className=' bg-DeepNightBlack text-LightGray w-full h-10 flex items-center justify-between px-2 lg:hidden relative'>
-          <div className='icon flex items-center gap-x-2' onClick={(e) => setIntro(!intro)}>
-            <span className='icon border-2 text-Green border-Green p-1 text-sm rounded-lg'>
+
+      <div className="lg:hidden">
+        <div className=" bg-DeepNightBlack text-LightGray w-full h-10 flex items-center justify-between px-2 lg:hidden relative">
+          <div
+            className="icon flex items-center gap-x-2"
+            onClick={(e) => setIntro(!intro)}
+          >
+            <span className="icon border-2 text-Green border-Green p-1 text-sm rounded-lg">
               <SlOptionsVertical />
             </span>
-            <div className='text-Snow absolute -right-1 -bottom-1'>
+            <div className="text-Snow absolute -right-1 -bottom-1">
               <FaMousePointer />
             </div>
           </div>
-          <div className='icon flex items-center gap-x-2' onClick={(e) => setIsOpen(!isOpen)}>
-            <span className='icon border-2 text-Green border-Green p-1 text-sm rounded-lg'>
-              {' '}
+          <div
+            className="icon flex items-center gap-x-2"
+            onClick={(e) => setIsOpen(!isOpen)}
+          >
+            <span className="icon border-2 text-Green border-Green p-1 text-sm rounded-lg">
+              {" "}
               <FaBars />
             </span>
           </div>
         </div>
       </div>
-      <div className='flex relative h-full justify-between gap-x-3'>
+      <div className="flex relative h-full justify-between gap-x-3">
         {/* left most side */}
         <div
           className={`w-64 h-screen left-0 lg:rounded-xl -top-10 lg:top-0 lg:left-0 lg:h-full overflow-hidden ${bgColor} ${textColor} shadow-2xl z-50 lg:flex flex-col  lg:relative ${
-            intro ? 'flex absolute' : 'hidden'
-          }`}>
+            intro ? "flex absolute" : "hidden"
+          }`}
+        >
           <Intro isOpen={intro} setIsOpen={setIntro} theme={theme} />
         </div>
         {/* overlay */}
-        {intro && <div onClick={(e) => setIntro(false)} className='fixed top-0 left-0  w-full h-full bg-black/50 backdrop-blur-[2px] z-40'></div>}
+        {intro && (
+          <div
+            onClick={(e) => setIntro(false)}
+            className="fixed top-0 left-0  w-full h-full bg-black/50 backdrop-blur-[2px] z-40"
+          ></div>
+        )}
 
         {/* middle of screen */}
-        <div className={`w-full h-auto lg:w-9/12 shadow-2xl ${bgColor} ${textColor}  relative overflow-auto overflow-x-hidden no-scrollbar`}>{children}</div>
+        <div
+          className={`w-full h-auto lg:w-9/12 shadow-2xl ${bgColor} ${textColor}  relative overflow-auto overflow-x-hidden no-scrollbar`}
+        >
+          {children}
+        </div>
 
         {/* right side */}
         {/* right side */}
-        <div className={`hidden lg:block absolute lg:w-20 lg:relative bg-DeepNightBlack shadow-2xl rounded-xl overflow-hidden`}>
-          <div onClick={(e) => setIsOpen(!isOpen)} className='bg-MidNightBlack text-Green hidden lg:flex items-center h-16 justify-center text-2xl '>
-            <span className='icon border-2 border-Green p-2 rounded-xl'>
-              {' '}
+        <div
+          className={`hidden lg:block absolute lg:w-20 lg:relative bg-DeepNightBlack shadow-2xl rounded-xl overflow-hidden`}
+        >
+          <div
+            onClick={(e) => setIsOpen(!isOpen)}
+            className="bg-MidNightBlack text-Green hidden lg:flex items-center h-16 justify-center text-2xl "
+          >
+            <span className="icon border-2 border-Green p-2 rounded-xl">
+              {" "}
               <FaBars />
             </span>
           </div>
-          <span className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 flex items-center justify-center text-center text-xl ${textColor}  font-extrabold tracking-widest`}>
+          <span
+            className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 flex items-center justify-center text-center text-xl ${textColor}  font-extrabold tracking-widest`}
+          >
             NavBar
           </span>
         </div>
