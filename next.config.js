d753606c68fe59348fc2307d0737d646d5fd7cb0
Austin Heller <me@aheller.com>
2024-04-15 12:57:10 -0600

    Try asset prefix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  output: 'export',
-  basePath: '/portfolio-ellen-next-js',
-  assetPrefix: '/portfolio-ellen-next-js',
-
+  output: "export",
+  assetPrefix: "/portfolio-ellen-next-js",
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+};
