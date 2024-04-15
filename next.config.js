f38275d0ea5d7b8408a7d9ea74cef1ef42f9a336
Ellen Furness <elasfern@gmail.com>
2024-04-15 14:28:12 -0400

    build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  output: 'export',
+  basePath: '/portfolio-ellen-next-js',
+  images: {
+    unoptimized: true
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
