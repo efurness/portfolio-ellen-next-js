8f9ec160820d158e18adc3ebf4a58dd7fa0e5acc
Ellen Furness <elasfern@gmail.com>
2024-04-21 18:52:50 -0600

    newsfeed

--- pages/news-feed.jsx
+++ pages/news-feed.jsx
@@ -1,11 +1,30 @@
 import BannerLayout from "../components/Common/BannerLayout";
 import Footer from "../components/Footer";
+import React, { useEffect, useState } from 'react';
+import Articles from '../components/Articles';
+import axios from 'axios';
 
 const NewsFeed = () => {
+const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const getArticles = async () => {
+            setLoading(true);
+            const res = await axios.get(` https://api.nytimes.com/svc/topstories/v2/home.json?api-key=P820OA3BVKJ4QezqHU7b85aAQkmjezfc`);
+            console.log(res);
+            setArticles(res.data.results);
+
+            setLoading(false);
+        };
+        getArticles();
+
+    }, []);
   return (
     <BannerLayout>
       <div className="grid justify items-center grid-flow-row md:grid-cols-2 grid-rows-auto gap-4 px-8 my-6">
         News feed goes here
+        <Articles loading={loading} articles={articles} />
       </div>
       <Footer />
     </BannerLayout>
