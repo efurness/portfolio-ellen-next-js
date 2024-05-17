1c260880977caa70485055b68569a623176089b3
Ellen Furness <elasfern@gmail.com>
2024-05-17 13:27:18 -0600

    hidden2

--- pages/layout.js
+++ pages/layout.js
@@ -33,7 +33,7 @@ export default function Layout(props) {
 
       <div className="lg:hidden">
         <div
-          className={`${bgColor} ${textColor} w-full h-10 flex items-center justify-between px-2 `}
+          className={`${bgColor} ${textColor} w-full h-10 flex items-center justify-between px-2 lg:hidden relative`}
         >
           <div
             className="icon flex items-center gap-x-2"
