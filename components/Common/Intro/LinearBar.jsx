55cb937c373ba997c9004ad6bf4cae4d4b696657
Ellen Furness <elasfern@gmail.com>
2024-05-20 20:27:37 -0600

    linearBar

--- components/Common/Intro/LinearBar.jsx
+++ components/Common/Intro/LinearBar.jsx
@@ -5,7 +5,7 @@ const LinearBar = ({ title, percent, bgColor  }) => {
     
     return (
         <div className="">
-            <div className="flex justify-between mb-1">
+            <div className="flex justify-between mb-1 spacer-sm">
                 <span className="text-xs ${textColor}">{title}</span>
                 <span className="text-xs ${textColor}">{percent}</span>
             </div>
