aae762ac2acd468e04fce7e030eef4443aca581d
Ellen Furness <elasfern@gmail.com>
2024-05-22 13:56:12 -0600

    pb125

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -6,13 +6,13 @@ const Download = ( props ) => {
 
   return (
     <>
-      <Link href='/Resume2024.pdf' target='_blank' className={`flex flex-row ${textColor} items-center gap-x-4 md:pb-14 pt-4 pb-[150px]`}>
+      <Link href='/Resume2024.pdf' target='_blank' className={`flex flex-row ${textColor} items-center gap-x-4 md:pb-14 pt-4 pb-[125px]`}>
         <span className={`${textColor}`}>Download Resume</span>
         <span>{props.icon}</span>
       </Link>
       {/* <div className='spacer-sm'></div> */}
     </>
   );
-};
+}
 
 export default Download;
