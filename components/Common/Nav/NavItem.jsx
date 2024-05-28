ec53f6c8c3257a3b53b288a89303e9bf94f10a8e
Ellen Furness <elasfern@gmail.com>
2024-05-28 15:41:31 -0600

    hoverColor

--- components/Common/Nav/NavItem.jsx
+++ components/Common/Nav/NavItem.jsx
@@ -1,25 +1,27 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router';
-import React from 'react'
-
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 
 const NavItem = ({ NavIcon, NavText, NavRoute, setIsOpen, theme }) => {
-    const textColor = theme === "dark" ? "text-Snow" : "text-blue-600";
-    const hoverColor = theme === "dark" ? "bg-LightGray" : "bg-SilverGray";
-  
-    const router = useRouter();
-    const className = router.asPath === `${NavRoute}` ? "rounded-xl !text-DeepNightBlack bg-Green font-bold tracking-widest" : '';
+  const textColor = theme === "dark" ? "text-Snow" : "text-blue-600";
+  const hoverColor = theme === "dark" ? "hover:bg-gray-700" : "hover:bg-blue-200";
 
-    return (
-        <Link
-            onClick={(e) => setIsOpen(false)}
-            href={NavRoute}
-            className={`${className} transition flex items-center px-2 hover:${textColor} ${textColor} text-semibold hover:${hoverColor} rounded-xl  py-1.5 font-semibold space-x-4 text-base`}
-        >
-            {NavIcon}
-            <span>{NavText}</span>
-        </Link>
-    )
-}
+  const router = useRouter();
+  const className =
+    router.asPath === `${NavRoute}`
+      ? "rounded-xl !text-DeepNightBlack bg-green-400 font-bold tracking-widest"
+      : "";
 
-export default NavItem
+  return (
+    <Link
+      onClick={(e) => setIsOpen(false)}
+      href={NavRoute}
+      className={`${className} transition flex items-center px-2 ${textColor} ${hoverColor} rounded-xl  py-1.5 font-semibold space-x-4 text-base`}
+    >
+      {NavIcon}
+      <span>{NavText}</span>
+    </Link>
+  );
+};
+
+export default NavItem;
