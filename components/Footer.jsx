173d38a89a0f18d002c5994426f4e875aaf6fef0
Ellen Furness <elasfern@gmail.com>
2024-05-30 22:45:32 -0600

    links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
           <div className="mr-1 text-base">
             <AiFillCopyrightCircle />
           </div>
-          <span>2024 All Rights Reserved.</span>
+          <span>2024</span>
         </div>
         <div className="flex items-center">
-        Made with ❤️  <span className="font-bold">Monty-Asp</span></div>
+        Made with ❤️  <span className="font-bold"> Monty-Asp</span></div>
 
           <a
             href="https://efurness.github.io/ellen-s-react-portfolio/"
@@ -32,6 +32,26 @@ const Footer = () => {
             <span className="text-right"><img src="images/storymap.png" alt="story map image" /></span>
             
           </a>
+          <a
+            href="https://efurness.github.io/ellen-s-react-portfolio/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            
+            <span className="text-right"><img src="images/resume.png" alt="resume image" /></span>
+            
+          </a>
+          <a
+            href="https://efurness.github.io/Ellen-sProfessionalPortfolio/photo.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            
+            <span className="text-right"><img src="images/camera.png" alt="camera image" /></span>
+            
+          </a>
+
+
 
         </div>
         <div className="hidden md:flex items-center">
