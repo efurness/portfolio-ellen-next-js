72208f372bc7990730979a86d6045b07be25f3b6
Ellen Furness <elasfern@gmail.com>
2024-06-16 13:31:50 -0600

    icon

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/portfolio-ellen-next-js" : "";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
       <body>
         <Main />
